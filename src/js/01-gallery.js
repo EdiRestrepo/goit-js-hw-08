84d4fd2c965bf737b7b8f2d5d85d4fff163c9dc4
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-02-06 10:05:37 -0500

    Se sube la tarea #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,38 +1,41 @@
-// Descrito en la documentación
-import SimpleLightbox from "simplelightbox";
-// Importación adicional de estilos
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
 
-// Add imports above this line
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const createItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+      `;
+  })
+  .join('');
 
-console.log(galleryItems);
-
-//list container selector
-const listGallery = document.querySelector(".gallery");
-
-//It is necessary to slightly change the gallery card markup; use this template.
-const galleryMarkup = galleryItems
-  .map(
-    (galleryItems) =>
-      `<a class="gallery__item" href="${galleryItems.original}">
-      <img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
-    </a>`
-  )
-  .join("\n");
-
-  //1. Creating and rendering markup from the galleryItems data array and provided gallery element template.
-//   Use the ready-made code from the first task.
-listGallery.insertAdjacentHTML("afterbegin", galleryMarkup);
-
-//3. Library initialization after gallery items are created and added to div.gallery. To do this, read the
-//   SimpleLightbox documentation - first of all, the Usage and Markup sections.
-//4. Look in the documentation for the Options section and add image caption display from the alt attribute.
-//   Let the caption be at the bottom and appear 250 milliseconds after image opening.
-listGallery.addEventListener("click", (event) => event.preventDefault());
-
+const alleryContainerEl = document.querySelector('.gallery');
+alleryContainerEl.insertAdjacentHTML('beforeend', createItemsMarkup);
 let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
- });
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+  doubleTapZoom: 1,
+});
+alleryContainerEl.addEventListener('click', event => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+});
+
+//console.log(galleryItems);
+
+
+
+
+
+
+
+
+
+
+
